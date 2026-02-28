cd3eaf749917906f07bbde9306c20bd12e530787
Vicky Enalen <enalenv@gmail.com>
2026-02-28 08:44:44 -0800

    Deployment updates

--- expense_tracker/server/src/db.ts
+++ expense_tracker/server/src/db.ts
@@ -2,6 +2,7 @@ import { Pool } from 'pg';
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ...(process.env.NODE_ENV === 'production' ? { ssl: { rejectUnauthorized: false } } : {}),
 });
 
 export default pool;
